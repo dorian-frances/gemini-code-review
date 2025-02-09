59c9d86b7fcda862e6706c23556731c6f5840fe7
dorian-frances <dorian.frances@ext.bpifrance.fr>
2025-02-09 23:57:25 +0100

    implement recommandation fetching through gemini model

--- src/review-my-code.tsx
+++ src/review-my-code.tsx
@@ -1,14 +1,50 @@
 import { Action, ActionPanel, Detail, List, showToast, Toast } from "@raycast/api";
-import { AsyncState, useFetch } from "@raycast/utils";
+import { AsyncState, useCachedState, useFetch } from "@raycast/utils";
 import { GEMINI_API_KEY } from "./adapter/gemini-adapter";
 import { GenerateContentResponse } from "@google/generative-ai";
 import { useEffect, useState } from "react";
 import { getBranches, getGitDiff } from "./adapter/git-command-adapter";
+import { format } from "./core/string-utils";
+import { log } from "node:util";
+
+export const prompt = "Given the following Git diff between a developer's branch and the origin/main branch, analyze the changes and provide a constructive code review. The goal is to help the developer identify improvements before submitting the code to a human reviewer.\\n\\n#### Git Diff\\n```\\ndiff\\n{0}\\n```\\n\\n### Review Guidelines\\n1. **Best Practices Compliance**: Identify deviations from standard coding practices, including naming conventions, SOLID principles, DRY, KISS, and other general coding guidelines.\\n2. **Code Quality**: Highlight areas related to readability, maintainability, and performance improvements.\\n3. **Bug Detection**: Detect potential logic errors or problematic patterns that may cause unintended behavior.\\n4. **Security Concerns**: Flag any security vulnerabilities or unsafe coding practices.\\n5. **Optimization Suggestions**: Provide insights on improving efficiency where applicable.\\n6. **Severity Categorization**: Classify each identified issue into one of the following levels:\\n   - **Critical**: Must be addressed before merging, as it may introduce security risks, major bugs, or serious performance issues (use a red circle emoji for this level).\\n   - **Major**: Strongly recommended fixes that impact maintainability, best practices, or potential performance problems (use an orange circle emoji for this level).\\n   - **Minor**: Non-blocking suggestions that improve readability, efficiency, or minor stylistic consistency (use a blue circle emoji for this level).\\n\\n### Response Format\\n- The response must be in **Markdown** and use headings starting from `##` (Heading 2).\\n- Each section headings needs to start with an emoji.\\n- Each issue should be categorized under the appropriate severity level.\\n- The review should be concise while still offering enough detail for the developer to understand the reasoning behind the feedback.\\n- No direct fixes should be provided—only suggestions and explanations.\\n\\n### Context Considerations\\n- The model should analyze **only the provided Git diff** without requiring broader project context.\\n- The review should be **language-agnostic**, adapting feedback based on common best practices for the detected programming language."
 
 export default function Command() {
-  const [branches, setBranches] = useState<string[]>([]);
+  const [branches, setBranches] = useCachedState<string[]>("local-branches", []);
   const [selectedBranch, setSelectedBranch] = useState<string | null>(null);
   const [diff, setDiff] = useState<string>("Select a branch to see the diff");
+  const [modelRecommendation, setModelRecommendation] = useState<string>("")
+  const [requestBody, setRequestBody] = useState<string | null>(null);
+  const [loadingReview, setLoadingReview] = useState<boolean>(false);
+  const { error, revalidate } = useFetch(
+    `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: requestBody,
+      execute: false,
+      onWillExecute: () => {
+        setModelRecommendation("⏳ Fetching model recommandation...")
+        setLoadingReview(true);
+      },
+      onData: (data: GenerateContentResponse) => {
+        if (data !== undefined) {
+          const candidateResponse = data.candidates?.at(0);
+          if (candidateResponse !== undefined) {
+            setModelRecommendation(candidateResponse.content.parts[0].text ?? "Aucune réponse obtenue.")
+            setLoadingReview(false);
+            return;
+          }
+        }
+        setModelRecommendation("Aucune réponse obtenue.")
+        setLoadingReview(false);
+      },
+      onError: (error)=> {
+        console.log(error);
+        setLoadingReview(false)
+      },
+    },
+  );
 
   useEffect(() => {
     try {
@@ -24,47 +60,32 @@ export default function Command() {
     }
   }, []);
 
-  const prompt = "Tell me something interesting about the history of AI";
-  const { isLoading, data, error }: AsyncState<string> = useFetch(
-    `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`,
-    {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        contents: [
-          {
-            parts: [{ text: prompt }],
-          },
-        ],
-      }),
-      mapResult: (data: GenerateContentResponse) => {
-        if (data !== undefined) {
-          const candidateResponse = data.candidates?.at(0);
-          if (candidateResponse !== undefined) {
-            return { data: candidateResponse.content.parts[0].text ?? "Aucune réponse obtenue." };
-          }
-        }
+  useEffect(() => {
+    if (requestBody) {
+      revalidate();
+    }
+  }, [requestBody]);
 
-        return {
-          data: "Aucune réponse obtenue.",
-        };
-      },
-    },
-  );
+  function handleBranchSelection(branch: string) {
+    const diffResult = getGitDiff(branch);
+    setSelectedBranch(branch);
+    setDiff(diffResult || "No changes found.");
+    setRequestBody(JSON.stringify({
+      contents: [
+        {
+          parts: [{ text: format(prompt, diffResult) }],
+        },
+      ],
+    }));
+  }
 
   if (error) {
     showToast({ style: Toast.Style.Failure, title: "Erreur", message: error.message });
     return <Detail markdown="### ❌ Une erreur est survenue lors de la récupération des données." />;
   }
 
-  function handleBranchSelection(branch: string) {
-    const diffResult = getGitDiff(branch);
-    setSelectedBranch(branch);
-    setDiff(diffResult || "No changes found.");
-  }
-
-  if (selectedBranch) {
-    return <Detail markdown={`## Diff for ${selectedBranch}\n\n\`\`\`diff\n${diff}\n\`\`\``} />;
+  if (selectedBranch)  {
+    return <Detail isLoading={loadingReview} markdown={`# Code review from Gemini for branch ${selectedBranch}\n${modelRecommendation}`} />;
   }
 
   return (
